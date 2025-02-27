c6b013b24d632c5db0afe47a242063d8f68ec8c0
TexTrue <65154269+TexBlock@users.noreply.github.com>
2025-02-27 19:19:52 +0800

    port to 1.20.1 fabric

--- src/main/java/com/rinko1231/armordamagelimit/config/ArmorProtectionConfig.java
+++ src/main/java/com/rinko1231/armordamagelimit/config/ArmorProtectionConfig.java
@@ -1,18 +1,19 @@
 package com.rinko1231.armordamagelimit.config;
 
-import net.minecraftforge.common.ForgeConfigSpec;
-import net.minecraftforge.fml.ModLoadingContext;
-import net.minecraftforge.fml.config.ModConfig;
+import com.rinko1231.armordamagelimit.ArmorDamageLimit;
+import fuzs.forgeconfigapiport.fabric.api.neoforge.v4.NeoForgeConfigRegistry;
+import net.neoforged.fml.config.ModConfig;
+import net.neoforged.neoforge.common.ModConfigSpec;
 
 import java.util.List;
 
 
 public class ArmorProtectionConfig
 {
-    public static ForgeConfigSpec SPEC;
-    public static final ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder();
-    public static ForgeConfigSpec.DoubleValue maxArmorDurabilityLossPercent;
-    public static ForgeConfigSpec.ConfigValue<List<? extends String>> itemProtectionBlacklist;
+    public static ModConfigSpec SPEC;
+    public static final ModConfigSpec.Builder BUILDER = new ModConfigSpec.Builder();
+    public static ModConfigSpec.DoubleValue maxArmorDurabilityLossPercent;
+    public static ModConfigSpec.ConfigValue<List<? extends String>> itemProtectionBlacklist;
 
     static
     {
@@ -31,8 +32,6 @@ public class ArmorProtectionConfig
 
     public static void setup()
     {
-        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, SPEC, "ArmorDamageLimit.toml");
+        NeoForgeConfigRegistry.INSTANCE.register(ArmorDamageLimit.MOD_ID, ModConfig.Type.COMMON, SPEC);
     }
-
-
 }
